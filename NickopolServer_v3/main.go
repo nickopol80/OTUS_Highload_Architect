f9db1aaae73d126aa1caae8a73f09d1121785d75
Slava Polyakov <pol_80@mail.ru>
2024-11-17 20:47:41 +0300

    Подготовка к запуску в докере

--- NickopolServer_v3/main.go
+++ NickopolServer_v3/main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"encoding/json"
 	"log"
-	// "os" // добавьте этот импорт, если его нет
+	"os" // добавьте этот импорт, если его нет
 
 	"github.com/gorilla/mux"
 
@@ -41,15 +41,15 @@ var customer = User{}
 func getDBConnection() (*sql.DB, error) {
 
 	// для запуска в контейнере
-	// dbUser := os.Getenv("DB_USER")
-    // dbPassword := os.Getenv("DB_PASSWORD")
-    // dbHost := os.Getenv("DB_HOST")
-    // dbPort := os.Getenv("DB_PORT")
-    // dbName := os.Getenv("DB_NAME")
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbName := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	// для отладки локално (не через контейнер)
-    dsn := "root:root@tcp(localhost:3306)/nickopolis"
+    // dsn := "root:root@tcp(localhost:3306)/nickopolis"
 
     db, err := sql.Open("mysql", dsn)
     if err != nil {
